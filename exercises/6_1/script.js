c038186402025b7af462ad2261945a359f1ef234
TerezaCarvalho <terezasimoes@gmail.com>
2020-04-22 20:43:24 -0300

    corrigindo erros

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,7 +1,5 @@
 let estadoSelect = document.getElementById('estado');
-
   function mostraEstados(){
-   
         let estados = [
         'Acre',
         'Alagoas',
@@ -27,55 +25,37 @@ let estadoSelect = document.getElementById('estado');
         'Santa Catarina',
         'São Paulo',
         ]
-
       for(let i=0; i<estados.length; i+=1 ){
-          let opcao = document.createElement('opcao');
+          let opcao = document.createElement('option');   // .createElement('option') 
           opcao.innerHTML=estados[i];
           estadoSelect.appendChild(opcao);
       }
-
     }
-
-
   mostraEstados();
-
-
  let  inputData = document.getElementById('data-entrada');
   function verificaData(){
-    let verificaDatas=inputData.nodeValue.split('/');
-    for(let i=o; i<verificaDatas.length; i+=1){
-        if(verificaDatas[0] <1 || verificaDatas > 31){
-            alert ("data inválida");
-        }else if(verificaDatas[1]<1 || verificaDatas >12){
-            alert("data inválida");
-        }else if(verificaDatas[2] <0){
-            alert("data inválida")
-        }
+    let verificaDatas=inputData.value.split('/');   // inputData.value     [10,05,2020]
+    if(verificaDatas[0] < 1 || verificaDatas[0] > 31){    // verificaDatas[0]
+        alert ("data inválida");
+    }else if(verificaDatas[1]<1 || verificaDatas[1] >12){  // verificaDatas[1]
+        alert("data inválida");
+    }else if(verificaDatas[2] < 0){
+        alert("data inválida")
     }
-
   }
-
   let inptEmail = document.getElementById('email');
-
   function verificaEmail(){
     let hieroglifos =  /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-    let email = inptEmail.nodeValue;
+    let email = inptEmail.value;
     if (hieroglifos.test(email)){
-        console.log("Email válido")
+        alert("Email válido")
     }else{
-        console.log("Email inválido")
+        alert("Email inválido")
     }
   }
-
-
-
   const btncadastro = document.getElementById('enviar');
   btncadastro.addEventListener('click', function (event) {
+    event.preventDefault();  // PRECISA ESTAR NO INICIO PRA NAO ZERAR OS DADOS DO FORMULARIO
     verificaData();
     verificaEmail();
-    event.preventDefault();
   });
-
-
-
-  
